8e78dd2fd3bf61400415adef83d36af21efb018c
James King <metapyziks@gmail.com>
2012-09-03 18:16:31 +0100

    Created project files

---
+++ ASMCellSim/Cell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASMCellSim
+{
+    class Cell
+    {
+        public const float Radius = 1.0f;
+        public const float RepulsionMultiplier = 0.5f;
+    }
+}
